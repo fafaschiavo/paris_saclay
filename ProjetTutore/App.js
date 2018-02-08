4839b84d942ce20c6b0d6d005d5ddedc111b2996
fabricio <fayschiavo@gmail.com>
2018-02-09 00:59:40 +0100

    first elements

--- ProjetTutore/App.js
+++ ProjetTutore/App.js
@@ -1,37 +1,83 @@
 import React, { Component } from 'react';
 import { StyleSheet, AppRegistry, Text, View, Image } from 'react-native';
+import { Font } from 'expo';
 
 const styles = StyleSheet.create({
 
-    example_container:{
+    full_screen_container:{
         width: '100%',
-        alignItems: 'center',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0
     },
 
-    bigblue: {
-        color: 'blue',
-        fontWeight: 'bold',
-        fontSize: 30,
+    background_image: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover',
     },
 
-    red: {
-        color: 'red',
+    foreground_container:{
+        width: '100%',
+        height: '100%',
+        top: '-100%'
     },
+
+    saclay_logo:{
+        position: 'absolute',
+        top: '3%',
+        left: '2%',        
+        width: '60%',
+        height: '10%',
+        resizeMode: 'contain',
+    },
+
+    home_text_title:{
+        fontFamily: 'montserrat-bold',
+        color: '#000000',
+        textAlign: 'center',
+    }
 
 });
 
 export default class HelloWorldApp extends Component {
-    render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    constructor(props) {
+        super(props);
+        this.state = {
+            fontsLoaded: false
         };
+    }
 
+    async componentDidMount(){
+        await Font.loadAsync({
+            'montserrat-black': require('./assets/fonts/Montserrat-Black.ttf'),
+            'montserrat-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+            'montserrat-light': require('./assets/fonts/Montserrat-Light.ttf'),
+            'montserrat-medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+            'montserrat-semibold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+            'montserrat-thin': require('./assets/fonts/Montserrat-Thin.ttf'),
+            'montserrat-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+        });
+
+        this.setState({ fontsLoaded: true });
+    }
+
+    render() {
         console.log('Here -------------------------------')
 
         return (
-            <View style={styles.example_container}>
-                <Image source={pic} style={{width: 193, height: 110}}/>
-                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+            <View style={styles.full_screen_container}>
+                <View style={styles.full_screen_container}>
+                    <Image style={styles.background_image} source={{ uri: 'https://s3.eu-west-3.amazonaws.com/paris-saclay/background_1-01.png' }} />
+                </View>
+                <View style={styles.full_screen_container}>
+                    <Image style={styles.saclay_logo} source={{ uri: 'https://s3.eu-west-3.amazonaws.com/paris-saclay/logo_paris_saclay_big.png' }} />
+                    {this.state.fontsLoaded &&
+                    <Text style={styles.home_text_title}>But now, it's a real mobile app</Text>
+                    }
+                </View>
             </View>
         );
     }
